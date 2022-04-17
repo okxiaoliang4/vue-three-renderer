74aa165c33f6e781d5b1018466c0a2aeda2189b1
okxiaoliang4 <353742991@qq.com>
2022-04-17 21:54:13 +0800

    chore(playground): update

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -17,24 +17,30 @@ useRafFn(() => {
   }
 })
 
-const color = ref(0x00FF00)
+const materialParameters = ref({
+  color: 0x00FF00,
+})
 
 function toggleColor() {
-  color.value = color.value === 0x00FF00 ? 0xFF0000 : 0x00FF00
+  materialParameters.value.color = materialParameters.value.color === 0x00FF00 ? 0xFF0000 : 0x00FF00
 }
 
 useIntervalFn(toggleColor, 1000)
+
+const boxHeight = ref(1)
 </script>
 
 <template>
-  <WebGLRenderer :camera="cameraRef?.camera" :scene="sceneRef?.scene" :width="width" :height="height" />
+  <div @click="boxHeight++">
+    <WebGLRenderer :camera="cameraRef?.camera" :scene="sceneRef?.scene" :width="width" :height="height" />
 
-  <Scene ref="sceneRef">
-    <Mesh ref="meshRef">
-      <BoxGeometry />
-      <MeshBasicMaterial :pameters="{ color }" />
-    </Mesh>
-  </Scene>
+    <Scene ref="sceneRef">
+      <Mesh ref="meshRef">
+        <BoxGeometry :width="1" :height="boxHeight" />
+        <MeshBasicMaterial :pameters="materialParameters" />
+      </Mesh>
+    </Scene>
 
-  <PerspectiveCamera ref="cameraRef" :fov="75" :aspect="width / height" :near="0.1" :far="1000" :position-z="5" />
+    <PerspectiveCamera ref="cameraRef" :fov="75" :aspect="width / height" :near="0.1" :far="1000" :position-z="5" />
+  </div>
 </template>
