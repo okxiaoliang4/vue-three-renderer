b2545ce45dce94e3b36f6f95f86de7d48791f1ed
okxiaoliang4 <353742991@qq.com>
2022-04-17 18:59:58 +0800

    chore(playground): update

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,20 +1,28 @@
 <script lang="ts" setup>
 import type SceneVue from '~/components/Scene.vue'
 import type PerspectiveCamera from '~/components/PerspectiveCamera.vue'
+import type MeshVue from '~/components/Mesh.vue'
 
 const width = window.innerWidth
 const height = window.innerHeight
 
 const sceneRef = ref<InstanceType<typeof SceneVue>>()
 const cameraRef = ref<InstanceType<typeof PerspectiveCamera>>()
+const meshRef = ref<InstanceType<typeof MeshVue>>()
 
+useRafFn(() => {
+  if (meshRef.value) {
+    meshRef.value.mesh.rotation.x += 0.01
+    meshRef.value.mesh.rotation.y += 0.01
+  }
+})
 </script>
 
 <template>
   <WebGLRenderer :camera="cameraRef?.camera" :scene="sceneRef?.scene" :width="width" :height="height" />
 
   <Scene ref="sceneRef">
-    <Mesh>
+    <Mesh ref="meshRef">
       <BoxGeometry />
       <MeshBasicMaterial :pameters="{ color: 0x00FF00 }" />
     </Mesh>
