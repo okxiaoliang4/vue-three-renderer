bce43751cff6b0b6d36acc66d25fe8f6d9c36888
okxiaoliang4 <353742991@qq.com>
2022-04-18 20:53:03 +0800

    chore: update demo

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,46 +1,51 @@
 <script lang="ts" setup>
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import type SceneVue from '~/components/Scene.vue'
 import type PerspectiveCamera from '~/components/PerspectiveCamera.vue'
 import type MeshVue from '~/components/Mesh.vue'
+import type WebGLRendererVue from '~/components/WebGLRenderer.vue'
 
 const width = window.innerWidth
 const height = window.innerHeight
 
+const rendererRef = ref<InstanceType<typeof WebGLRendererVue>>()
 const sceneRef = ref<InstanceType<typeof SceneVue>>()
 const cameraRef = ref<InstanceType<typeof PerspectiveCamera>>()
 const meshRef = ref<InstanceType<typeof MeshVue>>()
-
-useRafFn(() => {
-  if (meshRef.value) {
-    meshRef.value.mesh.rotation.x += 0.01
-    meshRef.value.mesh.rotation.y += 0.01
-  }
-})
 
 const materialParameters = ref({
   color: 0x00FF00,
 })
 
 function toggleColor() {
-  materialParameters.value.color = materialParameters.value.color === 0x00FF00 ? 0xFF0000 : 0x00FF00
+  materialParameters.value.color = materialParameters.value.color === 0x56DD88 ? 0x66F325 : 0x56DD88
 }
 
 useIntervalFn(toggleColor, 1000)
 
-const boxHeight = ref(1)
+onMounted(() => {
+  const controls = new OrbitControls(cameraRef.value!.camera, rendererRef.value?.renderer?.domElement)
+
+  useRafFn(() => {
+    controls.update()
+    if (meshRef.value) {
+      meshRef.value.mesh.rotation.x += 0.01
+      meshRef.value.mesh.rotation.y += 0.01
+    }
+  })
+})
+
 </script>
 
 <template>
-  <div @click="boxHeight++">
-    <WebGLRenderer :camera="cameraRef?.camera" :scene="sceneRef?.scene" :width="width" :height="height" />
+  <WebGLRenderer ref="rendererRef" :camera="cameraRef?.camera" :scene="sceneRef?.scene" :width="width" :height="height" />
 
-    <Scene ref="sceneRef">
-      <Mesh ref="meshRef">
-        <BoxGeometry :width="1" :height="boxHeight" />
-        <MeshBasicMaterial :pameters="materialParameters" />
-      </Mesh>
-    </Scene>
+  <Scene ref="sceneRef">
+    <Mesh ref="meshRef">
+      <BoxGeometry :width="1" :height="1" />
+      <MeshBasicMaterial :pameters="materialParameters" />
+    </Mesh>
+  </Scene>
 
-    <PerspectiveCamera ref="cameraRef" :fov="75" :aspect="width / height" :near="0.1" :far="1000" :position-z="5" />
-  </div>
+  <PerspectiveCamera ref="cameraRef" :fov="75" :aspect="width / height" :near="0.1" :far="1000" :position-z="5" />
 </template>
