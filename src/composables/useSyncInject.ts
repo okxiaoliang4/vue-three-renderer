1a844d4249cfc26a70c0ece366950501cf74f9d9
okxiaoliang4 <353742991@qq.com>
2022-05-02 20:47:38 +0800

    chore: update

--- src/composables/useSyncInject.ts
+++ src/composables/useSyncInject.ts
@@ -1,10 +1,11 @@
+import type { MaybeRef } from '@vueuse/core'
 import type { Ref } from 'vue'
 
-export function useSyncInject<T>(materialSymbol: Symbol, ref: Ref<T>) {
+export function useSyncInject<T>(materialSymbol: Symbol, material: MaybeRef<T>) {
   const parentMaterialRef = inject<Ref<T | null>>(materialSymbol)
   watchEffect(() => {
     if (parentMaterialRef)
-      parentMaterialRef.value = ref.value
+      parentMaterialRef.value = unref(material)
   })
 
   tryOnBeforeUnmount(() => {
